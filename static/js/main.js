6bbdd23e0686b5595e07a6abda617ff2bf9b4698
Hannes Anderes <hannes.anderes@gallus-group.com>
2016-10-11 16:43:31 +0200

    Add Style Changer functionality
    Signed-off-by: Hannes Anderes <hannes.anderes@gallus-group.com>

--- static/js/main.js
+++ static/js/main.js
@@ -8,7 +8,9 @@
 
     $(function() {
 
-        /* Register Handlebar Helperfunction to build a for loop */
+        /** 
+         *  Register Handlebar Helperfunction to build a for loop 
+         */
         Handlebars.registerHelper('for', function(count, options) {
           var ret = "";
           for(var i=0; i < count; i++) {
@@ -17,7 +19,9 @@
           return ret;
         });
 
-
+        /** 
+         *  Read notes from persistance and create html view
+         */
         function init() {
             /* Read note list from the persistance */
             var notes = noteRepo.getAll();
@@ -29,26 +33,31 @@
             $(".newNote").on('click', newNoteClickEventHandler);
         }
 
-        /*
-            This function creates table rows from the handlebar template 'node-template'
+        /**
+         *  This function creates table rows from the handlebar template 'node-template'
          */
         function createNoteList(notesList) {
             var createNodeList = Handlebars.compile($("#note-template").html());
             $(".noteList").empty();
             $(".noteList").append(createNodeList(notesList));
-            $(".editNote").on('click', editNoteClickEventHandler);
+            $(".editNote").on('click', editNoteClickEventHandler);            
+            $(".styleSelect select").on('change', changeStyleEventHandler);          
         }
 
 
-        /*
-           This function is an button event handler for the filter functionality
+        /**
+         *  This function is an button event handler for the filter functionality
          */
         function filterClickEventHandler(event) {
             var data = $(this).data();
             //Set all button backgrounds to default
             $(".filters").children().css({'background':'#3d94f6'});
+            //$(".filters").children().toggleClass('btn_defaultBackground');
+            //$(".filters").children().addClass('btn_inactive');
             //Set selected target to red
             $(event.target).css({'background':'red'});
+           // $(event.target).toggleClass('btn_active');
+            //$(event.target.className +  " .btn").toggleClass('btn_background');
             if (null != data) {
                 switch(data.filtertype) {
                     case "createdDate":
@@ -68,21 +77,39 @@
             }
         }
 
+        /**
+         *  This function is an button event handler to change the page to the new note
+         */
         function newNoteClickEventHandler(event) {
             window.location.href='../notes.html';
         }
 
+        /**
+         *  This function is an button event handler to change the page to the edit note
+         */
         function editNoteClickEventHandler(event) {
-
             var data = $(this).data();
             window.location.href='../notes.html?Page=edit&Key=' + data.uniqueid;
         }
 
         function finishedClickEventHandler(event) {
             /* Change backround color */
-            $(".showFinished .btn").toggleClass('btn_background');
+            $(".showFinished .btn").toggleClass('btn_active');
+            
             noteRepo.toggleShowOnlyFinished();
             createNoteList(noteRepo.getAll());
+        }
+        
+        function changeStyleEventHandler(event) {
+            var selectedStyle = $('.styleSelect option:selected').val();
+            if ("StyleOrange" == selectedStyle) {
+                $(".style").attr('href', "css/styleOrange.css");
+            } else if ("StyleBlackWhite" == selectedStyle) {
+                $(".style").attr('href', "");
+            } else {
+                /* Default style */
+                $(".style").attr('href', "css/styleOrange.css");
+            }                
         }
 
         init();
